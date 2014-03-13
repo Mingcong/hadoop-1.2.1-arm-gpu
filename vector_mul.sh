3b6253346c5aacb225e40839f0276699640e689d
Mingcong <songmingcong@gmail.com>
2014-03-13 13:51:42 -0400

    add shell script

---
+++ vector_mul.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CMDNAME=`basename $0`
+if [ $# -ne 1 ]; then
+    echo "Usage: $CMDNAME input" 1>&2
+    exit 1
+fi
+
+bin/hadoop dfs -rmr output
+
+bin/hadoop pipes -D hadoop.pipes.java.recordreader=true\
+ -D hadoop.pipes.java.recordwriter=true\
+ -output output\
+ -cpubin bin/gpu-vector_mul\
+ -gpubin bin/gpu-vector_mul\
+ -input $1
